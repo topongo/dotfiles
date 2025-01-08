55d27becb1e4e301e9f648bf1344fd0f155e6a99
topongo <lorenzo.bodini.private@gmail.com>
2025-01-08 17:42:13 +0100

    change workspaces widget, from literal to json for

--- eww/scripts/workspaces.sh
+++ eww/scripts/workspaces.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-BUTTON_TEMP="$(head -n 1 templates/workspaces.button.yuck | tr -d '\n')"
-BOX_TEMP="$(head -n 1 templates/workspaces.box.yuck | tr -d '\n')"
-
-function redraw() {
-  printf "$BOX_TEMP" "$(
-    SEL=$(hyprctl monitors -j | jq -r '.[0].activeWorkspace.id')
-    hyprctl workspaces -j | jq -r '.[].name' | sort | while read p; do
-      if [ "$p" == "$SEL" ]; then
-        printf "$BUTTON_TEMP" " wselected" "$p" "$p" 
-      else
-        printf "$BUTTON_TEMP" "" "$p" "$p"
-      fi
-    done
-  )"
-  echo
+function draw () {
+  ACTIVE=$(hyprctl monitors -j | jq -r '.[0].activeWorkspace.id')
+  hyprctl workspaces -j | jq -rc "map({\"index\": .id, \"name\": .id, \"selected\": ($ACTIVE == .id)})"
 }
 
-redraw
+draw
 
-socat -u UNIX:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read p; do
-  if echo $p | grep -qE "(|create|destroy)workspace"; then
-    redraw
-  fi
-done
-
-echo Exited normally $(date) >> /tmp/workspaces.log
- 
+socat \
+  -u UNIX:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | \
+  while read p; do
+    if echo $p | grep -qE '(|create|destroy)workspace'; then
+      draw "$p"
+    fi
+  done
