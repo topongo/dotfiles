0cde3dab133274d97d5f57e4d7624c877ee1f952
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-03-25 23:16:46 +0100

    fix(tuckr): symlinks cwd

--- Hooks/symlinks/post.sh
+++ Hooks/symlinks/post.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo Copying symlinks
-for link in $(ls ../Symlinks); do
-  cp --no-dereference --no-target-directory ../Symlinks/$link ~/$link
+cd ~/.config/dotfiles
+
+for link in $(ls Symlinks); do
+  cp --no-dereference --no-target-directory Symlinks/$link ~/$link
 done
