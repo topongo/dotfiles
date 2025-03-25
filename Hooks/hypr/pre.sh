be1d68dcd034c82e1cc00195aabc2f61e411b20b
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-03-25 23:17:36 +0100

    feat(tuckr): add check to hypr for private.conf

--- Hooks/hypr/pre.sh
+++ Hooks/hypr/pre.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-mkdir -p ~/.config/hypr
+# Ensure this is a directory
+[ -L ~/.config/hypr ] && exit 1
+[ -d ~/.config/hypr ] || mkdir -p ~/.config/hypr
+
+cd ~/.config/dotfiles
+
+# Ensure the `private.conf` file exists
+if ! [ -f Configs/hypr/.config/hypr/private.conf ]; then
+  echo "no such file: \`Configs/hypr/.configs/hypr/private.conf\`"
+  exit 1
+fi
