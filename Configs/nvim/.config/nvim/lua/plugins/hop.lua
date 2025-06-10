adf8726424207655b91b0e892af1b129cca75129
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-06-10 11:06:28 +0200

    nvim: hop.nvim: replace fFtT keybinds

--- Configs/nvim/.config/nvim/lua/plugins/hop.lua
+++ Configs/nvim/.config/nvim/lua/plugins/hop.lua
@@ -5,18 +5,32 @@ return {
   opts = {
     keys = "etovxqpdygfblzhckisuran",
   },
-  keys = {
-    { "<leader>hh", "<cmd>HopWordMW<cr>", desc = "Hop to word" },
-    { "<leader>hcc", "<cmd>HopChar1MW<cr>", desc = "Hop to char" },
-    { "<leader>hcv", "<cmd>HopChar2MW<cr>", desc = "Hop to pair of chars" }, 
-  }
--- config = function()
---     require("hop").setup()
---     local wk = require("which-key")
---     wk.add({
---       { "<leader>hh", "<cmd>HopWordMW<cr>", desc = "Hop to word" },
---       { "<leader>hcc", "<cmd>HopChar1MW<cr>", desc = "Hop to char" },
---       { "<leader>hcv", "<cmd>HopChar2MW<cr>", desc = "Hop to pair of chars" }, 
---     })
---   end
+  keys = function()
+    local hop = require('hop')
+    local directions = require('hop.hint').HintDirection
+
+    local keys = {
+      { "<leader>hh", "<cmd>HopWordMW<cr>", desc = "Hop to word" },
+      { "<leader>hcc", "<cmd>HopChar1MW<cr>", desc = "Hop to char" },
+      { "<leader>hcv", "<cmd>HopChar2MW<cr>", desc = "Hop to pair of chars" },
+      { "dH", "<cmd>HopLineMW<cr>"},
+    }
+
+    for k, v in pairs({
+      F = { drection = directions.BEFORE_CURSOR, current_line_only = true },
+      f = { direction = directions.AFTER_CURSOR, current_line_only = true },
+      T = { direction = directions.NEAREST_UP, current_line_only = true, hint_offset = 1 },
+      t = { direction = directions.NEAREST_DOWN, current_line_only = true, hint_offset = -1 },
+    }) do
+      table.insert(keys, {
+        k,
+        function()
+          hop.hint_char1({ direction = v, current_line_only = true })
+        end,
+        remap = true,
+        mode = '',
+      })
+    end
+    return keys
+  end,
 }
