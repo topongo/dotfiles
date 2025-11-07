eebe63be3339bb2c3755ca4ceaae8a8cdcae0cb7
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-11-07 13:41:52 +0100

    nvim: feat: better configs

--- Configs/nvim/.config/nvim/lua/plugins/oil.lua
+++ Configs/nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,18 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "icon",
+      "size",
+    }
+  },
+  view_options = {
+    show_hidden = true,
+  },
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   keys = {
     {"<C-n>", "<cmd>Oil<cr>", { noremap = true, silent = true }},
+    {"<C-S-n>", "<cmd>Oil --float<cr>", { noremap = true, silent = true }},
   }
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
