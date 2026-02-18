a9d3a8a88d8aee91ecc9202af32523c5f261084b
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2026-02-18 11:25:25 +0100

    feat: hypr: floating_bitwarden reverse behavior

--- Configs/hypr/.config/hypr/scripts/floating_bitwarden.sh
+++ Configs/hypr/.config/hypr/scripts/floating_bitwarden.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+# WARNING: from previous versions this script did opposite from now!
+#   - before: the script would find Bitwarden windows and made them floating
+#   -    now: the script finds firefox's windows and set them to tiling.
+#             by default all firefox windows now must be floating (see `Dependencies`)
+
 # Dependencies: socat, hyprland (duh)
 # To automatically load the script add it to your home and add this line to your `hyprland.conf':
 #   `exec-once = /path/to/floating_bitwarden.sh`
+#   `windowrule = match:class firefox, match:title Mozilla Firefox, float on`
 
 # read from hyprland socket2
 socat $XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | \
   while read p; do
-    # filter `windowtitlev2` events, meaning: "a title has changed it's title"
-    # then filter the title that must be exactly 'Extension: (Bitwarden Password Manager)'
+    # filter `windowtitlev2` events, from a window that has not "Mozilla Firefox" as his name.
+    if ! echo $p | grep -q 'windowtitlev2'; then
+      continue
+    elif echo $p | grep -q ',Mozilla Firefox'; then
+      continue
+    fi
+    # then for each window that HAS NOT exactly 'Extension: (Bitwarden Password Manager)' as the
+    # title we set disable floating:
     #   if we match only 'Bitwarden' every window that contains bitwarden will be matched
-    #   eg: searching 'Bitwarden' on google will trigger it
-    if echo $p | grep 'windowtitlev2' | grep 'Extension: (Bitwarden Password Manager)' -q; then
+    #   eg: searching 'Bitwarden' on google will not trigger it
+    if ! echo $p | grep 'windowtitlev2' | grep -q 'Extension: (Bitwarden Password Manager)'; then
       # get the id/address of the window that changed its title
       ADDR="0x$(echo $p | sed -E 's/.*>>(.*),.*/\1/')"
-      # set floating on
-      hyprctl dispatch setfloating address:$ADDR
-      # set window size to 400x600 pixels"
-      hyprctl dispatch resizewindowpixel "exact 400 600,address:$ADDR"
+      # set tiling on
+      echo $ADDR
+      hyprctl dispatch settiled address:$ADDR
     fi
   done
